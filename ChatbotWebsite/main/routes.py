47aa782f376723c6a03144668547bb8bb16e0dcf
Kundan547 <kundanvyas197@gmail.com>
2024-10-21 10:14:04 +0530

    Fixed the bug

--- ChatbotWebsite/main/routes.py
+++ ChatbotWebsite/main/routes.py
@@ -1,25 +1,23 @@
-from flask import render_template, Blueprint
+from flask import render_template
+from flask import Blueprint
 from flask_login import current_user, login_required
 
 main = Blueprint("main", __name__)
+
 
 # Home Page
 @main.route("/")
 def home():
     return render_template("home.html", title="Mental Health Chatbot")
 
+
 # About Page
 @main.route("/about")
 def about():
     return render_template("about.html", title="About")
 
+
 # SOS Page
 @main.route("/sos")
 def sos():
     return render_template("sos.html", title="SOS")
-
-# Journal Page (Restricted to logged-in users)
-@main.route("/journal")
-@login_required  # Ensures only logged-in users can access the journal
-def journal():
-    return render_template("journal.html", title="Journal", user=current_user)
