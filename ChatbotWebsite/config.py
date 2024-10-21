f570810d617186ee7fe9fef08331778b188947c1
Kundan547 <kundanvyas197@gmail.com>
2024-10-21 09:52:46 +0530

    First commit

--- ChatbotWebsite/config.py
+++ ChatbotWebsite/config.py
@@ -8,8 +8,8 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
     # contain mysql database url with user and password
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
-    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/users' #mysql
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'  #Alternative database if mysql not working
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/users' #mysql
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'  #Alternative database if mysql not working
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
